893f9b59abc657abfadb8fa818d6f20943ca2559
soroutmanisha2606 <soroutmanisha12@gmail.com>
2025-02-17 19:52:59 +0530

    fix logout issue

--- src/pages/navbar/Navbar.tsx
+++ src/pages/navbar/Navbar.tsx
@@ -2,31 +2,28 @@ import { FC, useState, useEffect } from 'react';
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './navbar.module.scss';
+import useAuthStore from '../../store/authStore';
 
 const Navbar: FC = () => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [userName, setUserName] = useState<string>('');
 
   useEffect(() => {
     // Check if user data exists in localStorage to determine login status
-    const userData = localStorage.getItem('userData');
-    console.log("userData", userData);
-    if (userData) {
+    const storedUserData = localStorage.getItem('userData');
+    if (storedUserData) {
+      const user = JSON.parse(storedUserData); // Parse user data from localStorage
       setIsLoggedIn(true);
+      setUserName(user.name); // Set the username
     } else {
       setIsLoggedIn(false);
     }
   }, []);
 
   const handleLogout = () => {
-    // Clear user data from localStorage
-    localStorage.removeItem('userData');
-    setIsLoggedIn(false);
-    // Use setTimeout to ensure state change happens before redirect
-    setTimeout(() => {
-      console.log(">>>>>>>>>>")
-      navigate('/login');
-    }, 100);
+    useAuthStore.getState().logout();
+    navigate('/login');
   };
 
   return (
@@ -35,10 +32,11 @@ const Navbar: FC = () => {
         <p>Script Assist</p>
       </div>
       <div className={styles.links}>
+        {isLoggedIn && userName && <p>Welcome, {userName}</p>} {/* Show username only if logged in */}
         {isLoggedIn ? (
           <button onClick={handleLogout} className={styles.link}>Logout</button>
         ) : (
-          <Link to="/login" className={styles.link}>Login</Link>
+          <></>
         )}
       </div>
     </div>
